fe678b962260946fb07f624510b8d489d99ea3cd
Astha <srivastavastha811@gmail.com>
2021-08-29 16:00:13 +0530

    created twitter client

--- src/main/java/kafka/twitter/TwitterProducer.java
+++ src/main/java/kafka/twitter/TwitterProducer.java
@@ -1,18 +1,77 @@
 package kafka.twitter;
 
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterProducer {
+
+    Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+
+    private String consumerKey = "9LQoli6ZPF3cudH22AIgnbiY2";
+    private String consumerSecret = "fnVrUYYcQ48JzHQ0TtIscbFSMoOpaFF1juHBd5J29oOjT4uZlc";
+    private String token = "956398472-A7Xx8NoN4lMANOD8oT2VDrVgyvwboZJgeKkEpNn7";
+    private String secret = "ArQGDAlnaBmJgG76ggGhEXm4agIjDjYhxsYZxeH2GgnlW";
+
     public static void main(String[] args) {
         new TwitterProducer().run();
     }
 
     private void run() {
+        logger.info("Setup...");
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(1000);
+        Client client = createTwitterClient(msgQueue);
+        client.connect();
 
+        // on a different thread, or multiple different threads....
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                logger.error("Error while polling in msgQueue");
+                client.stop();
+            }
+            if (msg != null)
+                logger.info(msg);
+        }
+        logger.info("End of application");
     }
 
-    public void createTwitterClient() {
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(10000);
+    public Client createTwitterClient(BlockingQueue<String> msgQueue) {
+
+        Hosts hoseBirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hoseBirdEndpoint = new StatusesFilterEndpoint();
+
+        List<String> terms = Lists.newArrayList("is");
+        hoseBirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        Authentication hoseBirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01") // optional: mainly for the logs
+                .hosts(hoseBirdHosts)
+                .authentication(hoseBirdAuth)
+                .endpoint(hoseBirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue)); // optional: use this if you want to process client events
+
+        return builder.build();
+
+
     }
 }
